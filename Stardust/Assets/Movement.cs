3450845db58ce2a269879ca7d975ea52c7fe0128
Mrpekonipvp <Lukaspietila37@gmail.com>
2023-01-11 21:43:05 +0200

    Levels added, script bugs still here

--- Stardust/Assets/Movement.cs
+++ Stardust/Assets/Movement.cs
@@ -5,49 +5,53 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private float horizontal;
-    private float speed=8f;
-    private float jumpingpower;
-    private bool isfacingright=true;
-    [SerializeField] private Rigidbody2D rb;
+    private float speed = 8f;
+    private float jumpingpower = 16f;
+    private bool isfacingright = true;
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private Layermask groundLayer;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = input.GetAxisRaw("Horizontal");
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isgrounded())
         {
-            rb.velocity = new Vector2(rb.veloclty.x, jumpingPower)
+            rb.velocity = new Vector2(rb.velocity.x, jumpingpower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
         Flip();
-
     }
     private void Fixedupdate()
     {
         rb.velocity = new Vector2D(horizontal * speed, rb.velocity.y);
     }
+    private bool isgrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
     private void Flip()
     {
-        if (isfacingright && horizontal < 0f || !isfacingright && horizontal > 0);
+        if (isfacingright && horizontal < 0f || !isfacingright && horizontal > 0f)
         {
             isfacingright = !isfacingright;
-            Vector3 localscale = transform.localscale;
-            localscale.x *= -1f;
-            transform.localscale = localscale;  
+            Vector3 localScale = Transform.localScale;
+            localScale.x *= -1f;
+            Transform.localScale = localScale;
         }
-
-    private bool isgrounded();
-        {
-            return Physics2D.Overlap(groundCheck.position, 0.2f, groundLayer);
-        }
-
+    }
 }
 
 
