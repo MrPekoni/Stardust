a76f13429a7931c721ecf0f918e4e3c97002fa01
MrPekoni <Lukaspietila37@gmail.com>
2023-01-11 17:32:21 +0200

    movement

--- Stardust/Assets/Movement.cs
+++ Stardust/Assets/Movement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private float horizontal;
+    private float speed=8f;
+    private float jumpingpower;
+    private bool isfacingright=true;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Layermask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,33 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.veloclty.x, jumpingPower)
+        }
+        Flip();
+
     }
+    private void Fixedupdate()
+    {
+        rb.velocity = new Vector2D(horizontal * speed, rb.velocity.y);
+    }
+    private void Flip()
+    {
+        if (isfacingright && horizontal < 0f || !isfacingright && horizontal > 0);
+        {
+            isfacingright = !isfacingright;
+            Vector3 localscale = transform.localscale;
+            localscale.x *= -1f;
+            transform.localscale = localscale;  
+        }
+
+    private bool isgrounded();
+        {
+            return Physics2D.Overlap(groundCheck.position, 0.2f, groundLayer);
+        }
+
 }
+
+
