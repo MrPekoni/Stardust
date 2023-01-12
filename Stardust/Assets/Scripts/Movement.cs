bb54efd09e5ff8302eb32bca049444b008954380
MrPekoni <Lukaspietila37@gmail.com>
2023-01-12 14:13:08 +0200

    More code and animations

--- Stardust/Assets/Scripts/Movement.cs
+++ Stardust/Assets/Scripts/Movement.cs
@@ -1,27 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private float horizontal;
+    public float horizontal;
     private float speed = 8f;
     private float jumpingpower = 16f;
     private bool isfacingright = true;
-    [SerializeField] private Transform groundCheck;
+    private Animator animator;
+    [SerializeField] private Transform Groundcheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        if (Input.GetButtonDown("Right"))
+        {
+            rb.velocity = 1;
+            animator.speed = 1;
+        }
+        if (Input.GetButtonDown("Left"))
+        {
+            rb.velocity = -1;
+            animator.speed = x;
+        }
         if (Input.GetButtonDown("Jump") && isgrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingpower);
@@ -31,15 +44,16 @@ public class Movement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
+        Console.WriteLine(horizontal);
         Flip();
     }
     private void Fixedupdate()
     {
-        rb.velocity = new Vector2D(horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
     private bool isgrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(Groundcheck.position, 0.2f, groundLayer);
     }
 
     private void Flip()
@@ -47,9 +61,9 @@ public class Movement : MonoBehaviour
         if (isfacingright && horizontal < 0f || !isfacingright && horizontal > 0f)
         {
             isfacingright = !isfacingright;
-            Vector3 localScale = Transform.localScale;
+            Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
-            Transform.localScale = localScale;
+            transform.localScale = localScale;
         }
     }
 }
