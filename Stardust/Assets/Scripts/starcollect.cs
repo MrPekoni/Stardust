d910aa2f7579c70583d90f040e0e72e11cb2b988
Mrpekonipvp <Lukaspietila37@gmail.com>
2023-01-15 06:01:10 +0200

    All levels done, star collection unfinished

--- Stardust/Assets/Scripts/starcollect.cs
+++ Stardust/Assets/Scripts/starcollect.cs
@@ -11,19 +11,19 @@ public class starcollect : MonoBehaviour
 	{
 		//Get the collider
 		box = GetComponent<BoxCollider2D>();
-		GameObject.Find("Player").GetComponent<Rigidbody2D>().gravityScale += -0.2f;
-		Physics2D.OverlapBox(Vector2 )
 	}
-	
+
+	private void OnCollisionEnter2D()
+	{
+		GameObject.Find("Player").GetComponent<Rigidbody2D>().gravityScale += -0.2f;
+		Debug.Log("Contact with Player made! Gravity reduced.");
+		GameObject.Destroy(GameObject.Find("Star"));
+	}
 	// Update is called once per frame
 	void Update()
 	{
-
+		var point = new Vector2(transform.position.x, transform.position.z);
+		var size = new Vector2(box.size.x, box.size.y);
+		Physics2D.OverlapBoxAll(point, size, 0f);
 	}
- //   private void OnCollisionEnter2D(Box,)
- //   {
-//		GameObject.Find("Player").GetComponent<Rigidbody2D>().gravityScale += -0.2f;
-//		Debug.Log("Contact with Player made! Gravity reduced.");
-//		GameObject.Destroy(gameObject);
- //   }
 }
